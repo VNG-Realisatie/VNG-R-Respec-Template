b79e74b5a9f2fcb78c6c142bcff8f51cebb184f3
melsk-r <robert.melskens@vng.nl>
2024-01-19 15:52:47 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -16,7 +16,6 @@ let respecConfig = {
   //previousMaturity: "IG",
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
-  prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/test/", "previousPublishVersion"],
   
   title: "Respec documentatie generatie",
   subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
