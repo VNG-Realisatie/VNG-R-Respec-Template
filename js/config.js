0b7eff26eb602b40b5e5a67e7ade25a27e2034ef
melsk-r <robert.melskens@vng.nl>
2024-01-22 09:23:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -18,9 +18,9 @@ let respecConfig = {
   //previousPublishVersion: "0.0.4",
 
 
-   latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName"],
+   latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/#"],
    // thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
-   thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/", "publishVersion"],
+   thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/", "publishVersion", "/#"],
    //prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
 //   prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/", "previousPublishVersion"],
 
